cd02bd8bd7a0780b21fbc858f68e4722079ba942
vandyand <vandyand@gmail.com>
2025-03-29 09:00:39 -0400

    Fix CORS issue by using relative API URL instead of localhost

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+const API_URL = process.env.REACT_APP_API_URL || "/api";
 
 // Create axios instance with base config
 const api = axios.create({
